2470c137a7e5318ea0698a89e8c08cdcbe80de66
malozaibi <ozaibitech@gmail.com>
2024-02-09 16:09:40 +0300

    lec5

---
+++ lec5/end/my_project.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 07, 2024 at 04:58 PM
+-- Server version: 8.1.0
+-- PHP Version: 8.1.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `id` int NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` text COLLATE utf8mb4_unicode_ci,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `blogs`
+--
+
+INSERT INTO `blogs` (`id`, `title`, `body`, `created_at`) VALUES
+(1, 'the begining', 'hello everyone <b>here</b>', '2024-02-02 15:54:14'),
+(2, 'other title', '<p>heloo o heloo heloo heloo </p>', '2024-02-02 15:55:19'),
+(3, 'The best PHP Course (not this)', 'Mohammed teach students a course that is not the best but not the worst', '2024-02-02 17:37:14'),
+(4, 'sss mmmm', 'mmm dddd', '2024-02-02 20:53:38'),
+(5, 'aaa', 'sdop jfkdsljf\r\nsdfklsdj fksa\r\nfsdklfjadsk;', '2024-02-02 20:53:45'),
+(6, 'aaa', 'sdop jfkdsljf\r\nsdfklsdj fksa\r\nfsdklfjadsk;', '2024-02-02 21:42:23'),
+(7, 'mohammed', 'mohsin', '2024-02-02 21:42:48'),
+(8, 'hi', 'kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> ', '2024-02-02 21:51:59'),
+(9, 'hi', 'kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> kldsjflskd fjkldsjf lksaj fk<b>ldskfjsdlkfj</b> ', '2024-02-02 21:52:58'),
+(10, 'the title of the blog', 'the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog ', '2024-02-02 21:53:54'),
+(11, 'the title of the blog', 'the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog the body of the blog ', '2024-02-02 21:54:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blogs`
+--
+ALTER TABLE `blogs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ lec5/start/admin/create_edit_blog.php
@@ -0,0 +1,73 @@
+<?php require_once('_partial/header.php') ?>
+<?php require_once '../core/functions.php'; ?>
+
+<?php
+$title = $_POST['title'] ?? '';
+$body = $_POST['body'] ?? '';
+$id = $_GET['id'] ?? null;
+$successFlag = false;
+if ($title && $body) {
+  $conn = getConnection();
+  try {
+    if ($id) {
+      $stmt = $conn->prepare("UPDATE blogs SET title = :title, body = :body WHERE id = :id");
+      $stmt->bindParam(':id', $id);
+    } else {
+      $stmt = $conn->prepare("INSERT INTO blogs (title, body) VALUES (:title, :body)");
+    }
+
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':body', $body);
+    $stmt->execute();
+
+    $successFlag = true;
+  } catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+  }
+}
+if ($id) {
+  $conn = getConnection();
+  $sql = "SELECT * FROM blogs WHERE id = :id LIMIT 1";
+  $stmt = $conn->prepare($sql);
+  // Bind the ID parameter
+  $stmt->bindParam(':id', $id);
+  $stmt->execute();
+  // Fetch the row
+  $row = $stmt->fetch(); // if no row return false
+}
+?>
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 mt-5">
+  <div class="col-md-6 mx-auto">
+    <form action="" method="post">
+      <h1 class="h3 mb-3 fw-normal"><?= $id ? "Update (#$id)" : 'Create' ?> Blog</h1>
+
+      <div class=" mb-2">
+        <label for="floatingInput">title</label>
+        <input type="text" class="form-control" name="title" id="floatingInput" value="<?= $id ? $row['title'] : '' ?>" />
+      </div>
+      <div class="mb-2">
+        <label for="textarea_input">Body</label>
+        <textarea class="form-control" name="body" id="textarea_input"><?= $id  ? $row['body'] : '' ?></textarea>
+      </div>
+
+      <button class="btn btn-primary w-100 py-2" type="submit">
+        <?= $id ? 'Update' : 'Add' ?>
+      </button>
+
+      <?php if ($successFlag) : ?>
+        <div class="alert alert-success"> <?= $id ? 'Updated' : 'Added' ?></div>
+      <?php endif; ?>
+    </form>
+
+    <?php if ($id) : ?>
+      <form action="destroy_blog.php" method="post">
+        <input type="hidden" name="id" value="<?= $id ?>">
+        <button class="btn btn-danger py-2" type="submit">
+          <?= 'Delete' ?>
+        </button>
+      </form>
+    <?php endif; ?>
+  </div>
+</main>
+
+<?php require_once('_partial/footer.php') ?>
